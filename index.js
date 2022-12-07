b92cec2d7c2761b7b061dd9e7e62fc32d9b7d735
Luke Harby <Luke.Harby@akqa.com>
2022-12-07 11:29:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+import fs from "fs";
+import fsExtra from "fs-extra";
+import client from "https";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+
+let images = [];
+const iterations = 3;
+
+const getImages = () => {
+  for (let i = 0; i <= iterations; i++) {
+    axios
+      .get("https://dog.ceo/api/breeds/image/random")
+      .then((result) => {
+        images.push(result.data.message);
+        if (i === iterations) {
+          cleanDirectory();
+          callback();
+        }
+      })
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+        if (i === iterations) {
+          getRandomSavedImage();
+        }
+      });
+  }
+};
+
+const callback = () => {
+  console.log("images before", images, images.length);
+  const rndNum = Math.floor(Math.random() * images.length);
+  const singleImage = images[rndNum];
+  downloadImage(singleImage, "./img/output.jpg");
+  images.splice(rndNum, 1);
+  console.log("images after: ", images, images.length);
+  downLoadimages();
+};
+
+function downloadImage(url, filepath) {
+  client.get(url, (res) => {
+    res.pipe(fs.createWriteStream(filepath));
+    res.on(`error`, (e) => {
+      console.log(`Error fetching images ${err}`);
+      reject(e);
+    });
+  });
+}
+
+const downLoadimages = () => {
+  images.forEach((item) => {
+    const fileName = uuidv4() + ".jpg";
+    return downloadImage(item, "./img-archive/" + fileName);
+  });
+};
+
+const getRandomSavedImage = () => {
+  cleanDirectory();
+  fs.readdir("./img-archive/", (err, files) => {
+    if (err) {
+      console.log(`Error reading random image from archive: ${err}`);
+    } else {
+      const rndNum = Math.floor(Math.random() * files.length);
+      const singleImage = files[rndNum];
+      fsExtra.move(
+        "./img-archive/" + singleImage,
+        "./img/" + singleImage,
+        function (err) {
+          if (err) {
+            console.error("err: ", err);
+          } else {
+            console.log(`src files copied`);
+          }
+        }
+      );
+    }
+  });
+};
+
+const cleanDirectory = () => {
+  fsExtra.emptyDirSync("./img/");
+};
+
+getImages();
